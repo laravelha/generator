46d57d25c18d33cd87135a7f5ace2dd6e45a5483
Pedro Augusto <pams.pedro@gmail.com>
2020-01-05 00:04:01 -0300

    feat: create migration syntax builder

---
+++ src/SyntaxBuilders/MigrationSyntaxBuilder.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace Laravelha\Generator\SyntaxBuilders;
+
+use Laravelha\Generator\GeneratorException;
+
+class MigrationSyntaxBuilder extends AbstractSintaxBuilder
+{
+    /**
+     * Create the PHP syntax for the given schema.
+     *
+     * @param  array $schema
+     * @param  array $meta
+     * @return array
+     * @throws GeneratorException
+     */
+    public function create(array $schema, array $meta): array
+    {
+        $up = $this->createSchemaForUpMethod($schema, $meta);
+        $down = $this->createSchemaForDownMethod($schema, $meta);
+
+        return compact('up', 'down');
+    }
+
+    /**
+     * Create the schema for the "up" method.
+     *
+     * @param  array $schema
+     * @param  array $meta
+     * @return string
+     * @throws GeneratorException
+     */
+    private function createSchemaForUpMethod(array $schema, array $meta): string
+    {
+        $fields = $this->constructSchema($schema);
+
+        if ($meta['action'] == 'create') {
+            return $this->insert($fields)->into($this->getCreateSchemaWrapper());
+        }
+
+        if ($meta['action'] == 'add') {
+            return $this->insert($fields)->into($this->getChangeSchemaWrapper());
+        }
+
+        if ($meta['action'] == 'remove') {
+            $fields = $this->constructSchema($schema, 'Drop');
+
+            return $this->insert($fields)->into($this->getChangeSchemaWrapper());
+        }
+
+        // Otherwise, we have no idea how to proceed.
+        throw new GeneratorException;
+    }
+
+    /**
+     * Construct the syntax for a down field.
+     *
+     * @param  array $schema
+     * @param  array $meta
+     * @return string
+     * @throws GeneratorException
+     */
+    private function createSchemaForDownMethod(array $schema, array $meta): string
+    {
+        // If the user created a table, then for the down
+        // method, we should drop it.
+        if ($meta['action'] == 'create') {
+            return sprintf("Schema::dropIfExists('%s');", $meta['table']);
+        }
+
+        // If the user added columns to a table, then for
+        // the down method, we should remove them.
+        if ($meta['action'] == 'add') {
+            $fields = $this->constructSchema($schema, 'Drop');
+
+            return $this->insert($fields)->into($this->getChangeSchemaWrapper());
+        }
+
+        // If the user removed columns from a table, then for
+        // the down method, we should add them back on.
+        if ($meta['action'] == 'remove') {
+            $fields = $this->constructSchema($schema);
+
+            return $this->insert($fields)->into($this->getChangeSchemaWrapper());
+        }
+
+        // Otherwise, we have no idea how to proceed.
+        throw new GeneratorException;
+    }
+
+    /**
+     * Store the given template, to be inserted somewhere.
+     *
+     * @param  string $template
+     * @return MigrationSyntaxBuilder
+     */
+    private function insert(string $template): MigrationSyntaxBuilder
+    {
+        $this->template = $template;
+
+        return $this;
+    }
+
+    /**
+     * Get the stored template, and insert into the given wrapper.
+     *
+     * @param  string $wrapper
+     * @param  string $placeholder
+     * @return string
+     */
+    private function into(string $wrapper, string $placeholder = 'schema_up'): string
+    {
+        return str_replace('{{' . $placeholder . '}}', $this->template, $wrapper);
+    }
+
+    /**
+     * Get the wrapper template for a "create" action.
+     *
+     * @return string
+     */
+    private function getCreateSchemaWrapper(): string
+    {
+        return file_get_contents(self::STUB_DIR.'/database/migrations/schema-create.stub');
+    }
+
+    /**
+     * Get the wrapper template for an "add" action.
+     *
+     * @return string
+     */
+    private function getChangeSchemaWrapper(): string
+    {
+        return file_get_contents(self::STUB_DIR.'/database/migrations/schema-change.stub');
+    }
+
+    /**
+     * Construct the schema fields.
+     *
+     * @param  array $schema
+     * @param  string $direction
+     * @return string|array
+     */
+    private function constructSchema(array $schema, string $direction = 'Add')
+    {
+        if (!$schema) return '';
+
+        $fields = array_map(function ($field) use ($direction) {
+            $method = "{$direction}Column";
+
+            return $this->$method($field);
+        }, $schema);
+
+        return implode("\n" . str_repeat(' ', 12), $fields);
+    }
+
+
+    /**
+     * Construct the syntax to add a column.
+     *
+     * @param  array $field
+     * @return string
+     */
+    private function addColumn(array $field): string
+    {
+        $syntax = sprintf("\$table->%s('%s')", $field['type'], $field['name']);
+
+        // If there are arguments for the schema type, like decimal('amount', 5, 2)
+        // then we have to remember to work those in.
+        if ($field['arguments']) {
+            $syntax = substr($syntax, 0, -1) . ', ';
+
+            $syntax .= implode(', ', $field['arguments']) . ')';
+        }
+
+        foreach ($field['options'] as $method => $value) {
+            $syntax .= sprintf("->%s(%s)", $method, $value === true ? '' : $value);
+        }
+
+        return $syntax .= ';';
+    }
+
+    /**
+     * Construct the syntax to drop a column.
+     *
+     * @param  string $field
+     * @return string
+     */
+    private function dropColumn($field)
+    {
+        return sprintf("\$table->dropColumn('%s');", $field['name']);
+    }
+}
