afbf61d6d4261bcd00830ce1fcb9f3052ba99e64
Pedro Augusto <pams.pedro@gmail.com>
2020-01-05 00:03:05 -0300

    feat: create abstract sintaxe builder

---
+++ src/SyntaxBuilders/AbstractSintaxBuilder.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Laravelha\Generator\SyntaxBuilders;
+
+class AbstractSintaxBuilder
+{
+    /**
+     *  Root dir of stubs
+     */
+    const STUB_DIR = __DIR__ . '/../stubs/';
+
+    /**
+     * A template to be inserted.
+     *
+     * @var string
+     */
+    protected $template;
+
+    /**
+     * String types supported by bluePrint
+     *
+     * @var array
+     */
+    protected $stringTypes;
+
+    /**
+     * Integer types supported by bluePrint
+     *
+     * @var array
+     */
+    protected $integerTypes;
+
+    /**
+     * Float types supported by bluePrint
+     *
+     * @var array
+     */
+    protected $floatTypes;
+
+    /**
+     * Date types supported by bluePrint
+     *
+     * @var array
+     */
+    protected $dateTypes;
+
+    /**
+     * All types supported by bluePrint
+     *
+     * @var array
+     */
+    protected $bluePrintTypes;
+
+
+    /**
+     * AbstractSintaxBuilder constructor.
+     */
+    public function __construct()
+    {
+        $this->stringTypes = config('ha-generator.stringTypes');
+        $this->integerTypes = config('ha-generator.integerTypes');
+        $this->floatTypes = config('ha-generator.floatTypes');
+        $this->dateTypes = config('ha-generator.dateTypes');
+        $this->bluePrintTypes = config('ha-generator.bluePrintTypes');
+    }
+
+    /**
+     * Determine if field has foreign constraint.
+     *
+     * @param  array $field
+     * @return bool
+     */
+    protected function hasForeignConstraint(array $field): bool
+    {
+        return 'foreign' === $field['type'];
+    }
+
+    /**
+     * Remove empty fields
+     * @param array $fields
+     * @return array
+     */
+    protected function removeEmpty(array $fields): array
+    {
+        foreach ($fields as $key => $field) {
+            if($field == '')
+                unset($fields[$key]);
+
+        }
+
+        return $fields;
+    }
+}
